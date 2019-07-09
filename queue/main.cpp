327c73ff67dece6e6475e4c22633ec42781c5674
chandan <chandan@chandan-ESPRIMO-P400>
2019-07-09 15:22:21 +0530

    hi yo

---
+++ queue/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 09-Jul-2019
+ *      Author: chandan
+ */
+#include<iostream>
+#include<bits/stdc++.h>
+#define SIZE 5
+using namespace std;
+class queue{
+private:
+	int arr[SIZE];
+	int count;
+public:
+	void addAtFirst(int num)
+	{
+
+	}
+};
+int main()
+{
+	cout<<"trying building queue"<<endl;
+	return 0;
+}
+
+
+
