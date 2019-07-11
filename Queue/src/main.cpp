1059898c2d6f92a2c81c396734f28d38c6fd0286
chandan singh <chandankds@gmail.com>
2019-07-11 17:30:28 +0530

    buggy code with rear not incrementing

---
+++ Queue/src/main.cpp
@@ -0,0 +1,59 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 08-Jul-2019
+ *      Author: chandan
+ */
+#include"queue.h"
+
+int main(void)
+{
+	queue q;
+
+	while(1)
+	{
+		int choice = menu();
+		switch(choice)
+		{
+		case EXIT: exit(0);
+
+		case ENQUEUE: //push ele into the queue at rear position
+//			if( !q.is_queue_full())
+//			{
+				int ele;
+				cout << "enter an ele: ";
+				cin >> ele;
+				q.enqueu(ele);
+				cout << ele << " inserted into the queue successfully..." << endl;
+//			}
+//			else
+//				cout << "queue overflow !!!" << endl;
+//			break;
+
+		case DEQUEUE: //delete ele from the queue which is at front pos
+			if( !q.is_queue_empty())
+			{
+				int ele = q.get_front();
+				q.dequeue();
+				cout << "deleted ele is : " << ele << endl;
+			}
+			else
+				cout << "queue underflow !!!" << endl;
+			break;
+
+		case GETFRONT://get the value of an ele at the front position
+			if( !q.is_queue_empty())
+			{
+				int ele = q.get_front();
+				cout << "ele at front position is : " << ele << endl;
+			}
+			else
+				cout << "queue underflow !!!" << endl;
+			break;
+		case PRINT: q.print_queue();
+			break;
+		}
+	}
+	return 0;
+}
+
---
+++ Queue/src/queue.cpp
@@ -0,0 +1,107 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 08-Jul-2019
+ *      Author: chs
+ */
+#include"queue.h"
+
+	queue::queue()
+	{
+		size = SIZE;
+		arr = new int[size];
+		this->front = -1;
+		this->rear = -1;
+
+	}
+
+	void queue::print_queue(){
+		for(int i=front ; i< ( rear + 1 ) % size; i++){
+			cout<<arr[i]<<"->";
+		}
+		cout<<endl;
+	}
+
+	bool queue:: is_queue_empty()
+	{
+		return (rear == -1 && front == rear );
+	}
+
+	bool queue:: is_queue_full()
+	{
+		return (front == (rear+1) % size );
+	}
+
+	void queue:: enqueu(int ele)
+	{
+		if(is_queue_full()){
+			resize();
+		}
+		else{
+		//- increment value of rear by 1
+			rear = ( rear + 1 ) % size;
+			//- push ele into the queue at rear position
+			cout<<"value of rear is now :: " << rear << "value of size is now :: " << size<< endl;
+			arr[ rear ] = ele;
+			cout<<"element is added to the rear"<< endl;
+			if( front == -1 )
+				front = 0;
+		}
+	}
+
+	void queue:: dequeue(void)
+	{
+		//if we are deleting last ele -- reinitialize queue
+		if( front == rear )
+			front = rear = -1;
+
+		else//- increment the value of front by 1 -- delete ele from queue
+		front = (front+1) % size;
+	}
+
+	int queue:: get_front(void)
+	{
+		return ( arr[ front ] );
+	}
+
+	void queue:: resize(){
+		int* biggi = new int[size*2];
+		for(int i=front; i<( rear + 1 ) % size; i++)
+		{
+			biggi[i] = arr[i];
+		}
+		delete arr;
+		arr = biggi;
+		size = size *2;
+	}
+
+
+	int menu()
+	{
+		int choice;
+		cout << "CIRCULAR QUEUE:" << endl;
+		cout << "0. EXIT" << endl;
+		cout << "1. ENQUEUE" << endl;
+		cout << "2. DEQUEUE" << endl;
+		cout << "3. GETFRONT" << endl;
+		cout << "4. PRINT" << endl;
+		cout << "enter the choice: ";
+		cin >> choice;
+		return choice;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
